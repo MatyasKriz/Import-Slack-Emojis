bed41207afc48779eb32b6c946def5871d33658f
Matyáš Kříž <kriz.matyas8.10@gmail.com>
2017-07-12 10:14:37 +0200

    Add argument parsing and try to add only files with familiar extensions.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import re
 import time
 import sys
 import signal
-from os import walk
+import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -17,9 +17,9 @@ class Emoji:
         self.path = path
 
 
-def signal_handler(signal, frame):
-    if signal is signal.SIGINT:
-        print('That\'s okay, see you later!')
+def signal_handler(sig, frame):
+    if sig is signal.SIGINT:
+        print('See you later!')
         sys.exit(1)
 
 signal.signal(signal.SIGINT, signal_handler)
@@ -33,17 +33,57 @@ def add_emoji(driver, emoji):
     text_element.send_keys(emoji.name, Keys.RETURN)
 
 
-team = input('Team name: ')
-path = input('Path: /Users/')
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--team', help='Name of the team for which you wish to add emojis.')
+parser.add_argument('-p', '--path', help='Path to the emoji directory.')
+args = parser.parse_args()
+
+if args.team:
+    team = args.team
+else:
+    team = input('Team name: ')
+team = team.replace(' ', '').lower()
+
+while True:
+    if args.path:
+        path = args.path
+    else:
+        path = '/Users/'
+        path += input('Path: /Users/')
+
+    if os.path.exists(path):
+        break
+    else:
+        print('Invalid path, try again')
+        path = None
+        args.path = None
+
+
+print('Please log in when the browser opens.')
+time.sleep(2)
 driver = webdriver.Chrome()
 driver.get('https://{}.slack.com/customize/emoji'.format(team))
 
-while "Custom Emoji" not in driver.title:
+while 'Custom Emoji' not in driver.title:
+    if 'slack.com' not in driver.current_url:
+        print('Navigated out of slack website, quitting...')
+        driver.close()
+        sys.exit(1)
     time.sleep(1)
 
-_, _, filenames = next(walk(path), (None, None, []))
+known_extensions = ['png', 'jpg', 'gif']
+_, _, files = next(os.walk(path), (None, None, []))
 no_extension = re.compile(r"^([^.]*).*")
-for filename in filenames:
-    emoji_name = re.search(no_extension, filename).group(1)
-    emoji = Emoji(emoji_name, path + '/' + filename)
+for file in files:
+    filepath = path + '/' + file
+    tmpArray = file.split('.')
+    filename = tmpArray[0]
+    extension = tmpArray[1]
+    if extension not in known_extensions:
+        print(extension)
+        continue
+
+    emoji = Emoji(filename, filepath)
     add_emoji(driver, emoji)
+
+driver.close()
